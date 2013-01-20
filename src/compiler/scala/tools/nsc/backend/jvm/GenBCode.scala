bec1ac940349615f173584f55b1342f133f0fee0
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-01-20 14:12:01 +0100

    bug fix

--- src/compiler/scala/tools/nsc/backend/jvm/GenBCode.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/GenBCode.scala
@@ -2129,11 +2129,26 @@ abstract class GenBCode extends BCodeOptInter {
 
                   } // end of genNormalMethodCall()
 
-              // TODO only if optimizing
+              // inter-procedural optimization: directly target final method in trait
               val detour = mixer.detouredFinalTraitMethods.getOrElse(sym, null)
               if(detour != null) {
-                log(s"Detouring from $sym to $detour")
-                genLoadQualifier(fun)
+                val selfRefBType          = toTypeKind(detour.firstParam.tpe)
+                val Select(qualifier, _)  = fun
+                val qualifierExpectedType = tpeTK(qualifier)
+                val isCastOutcomeKnown    = exemplars.get(selfRefBType).isSubtypeOf(qualifierExpectedType)
+                log(
+                  s"Rewiring to directly invokestatic final method $detour in trait $qualifierExpectedType , " +
+                  s"used to be an invokeinterface to $sym"
+                )
+                genLoad(qualifier, qualifierExpectedType)
+                val lastInsn = mnode.instructions.getLast
+                if(lastInsn.getOpcode == asm.Opcodes.CHECKCAST) {
+                  val ti = lastInsn.asInstanceOf[asm.tree.TypeInsnNode]
+                  if(lookupRefBType(ti.desc) != selfRefBType) {
+                    log(s"As part of rewiring a final trait method (SI-4767), removed ${insnPosInMethodSignature(ti, mnode, cnode)} ")
+                    mnode.instructions.remove(lastInsn)
+                  }
+                }
                 genLoadArguments(args, paramTKs(app))
                 genCallMethod(detour, Static(false), detour.owner, app.pos)
               } else {
