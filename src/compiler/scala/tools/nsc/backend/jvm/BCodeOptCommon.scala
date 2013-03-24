7d5af4b9865c6f61e8eb344f8cf428e7fbdc2088
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-03-24 22:04:59 +0100

    Late Anonymous-Closure Classes: GenBCode dehydrates them (+ TODO)
    Transform a Function node
      (x_1,...,x_n) => body
    of type
      FunctionN[T_1, .., T_N, R]
    into a Block expression of the form:
    
        {
          def hoisted(x_1: T_1, ..., x_N: T_n): R = body
    
          hoisted(zeroes-for-params-above).asInstanceOf[AbstractFunctionN[T_1, .., T_N, R]]
        }
    
    GenBCode will emit either:
    
      (a) an anonymous closure class and its instantiation ( under -closurify:delegating )
          or
      (b) a method handle given as constructor-argument to a closure instantiation ( under -closurify:MH )
    
    That way, the compiler has less work to do during specialize, explicitouter, erasure, and lambdalift, among others.
    
    The resulting anon-closure-classes have smaller constant pools, which significantly shrinks output size (good for mobile devices).
    Further classfile-size reductions can be obtained using -neo:o1 (ie intra-method optimizations, which lower the instruction count).
    
    TODO
      compiling test\files\continuations-run\t3223.scala
      with options -P:continuations:enable -Xpluginsdir build\quick\misc\scala-devel\plugins -Xplugin-require:continuations
      trips an assertion in GenBCode.

---
+++ src/compiler/scala/tools/nsc/backend/jvm/BCodeOptCommon.scala
@@ -0,0 +1,79 @@
+/* NSC -- new Scala compiler
+ * Copyright 2005-2012 LAMP/EPFL
+ * @author  Martin Odersky
+ */
+
+
+package scala.tools.nsc
+package backend
+package jvm
+
+import scala.tools.asm
+import asm.Opcodes
+import asm.optimiz.{ProdConsAnalyzer, UnBoxAnalyzer, Util}
+import asm.tree.analysis.SourceValue
+import asm.tree._
+
+import scala.collection.{ mutable, immutable }
+import scala.Some
+import collection.convert.Wrappers.JListWrapper
+import collection.convert.Wrappers.JSetWrapper
+
+/*
+ *  Utilities shared across intra-method and inter-procedural optimizations.
+ *
+ *  @author  Miguel Garcia, http://lamp.epfl.ch/~magarcia/ScalaCompilerCornerReloaded/
+ *  @version 1.0
+ *
+ */
+abstract class BCodeOptCommon extends BCodeTypes {
+
+  trait BCodeCleanserIface {
+    def intraMethodFixpoints(full: Boolean)
+  }
+
+  trait QuickCleanserIface {
+    def basicIntraMethodOpt(mnode: asm.tree.MethodNode)
+  }
+
+  //--------------------------------------------------------
+  // helpers to produce logging messages
+  //--------------------------------------------------------
+
+  final def methodSignature(ownerIName: String, methodName: String, methodDescriptor: String): String = {
+    ownerIName + "::" + methodName + methodDescriptor
+  }
+
+  final def methodSignature(ownerBT: BType, methodName: String, methodDescriptor: String): String = {
+    methodSignature(ownerBT.getInternalName, methodName, methodDescriptor)
+  }
+
+  final def methodSignature(ownerBT: BType, methodName: String, methodType: BType): String = {
+    methodSignature(ownerBT.getInternalName, methodName, methodType.getDescriptor)
+  }
+
+  final def methodSignature(ownerIName: String, mnode: MethodNode): String = {
+    methodSignature(ownerIName, mnode.name, mnode.desc)
+  }
+
+  final def methodSignature(ownerBT: BType, mnode: MethodNode): String = {
+    methodSignature(ownerBT, mnode.name, mnode.desc)
+  }
+
+  final def methodSignature(cnode: ClassNode, mnode: MethodNode): String = {
+    methodSignature(lookupRefBType(cnode.name), mnode.name, mnode.desc)
+  }
+
+  final def insnPos(insn: AbstractInsnNode, mnode: MethodNode): String = {
+   s"${Util.textify(insn)} at index ${mnode.instructions.indexOf(insn)}"
+  }
+
+  final def insnPosInMethodSignature(insn: AbstractInsnNode, mnode: MethodNode, cnode: ClassNode): String = {
+    insnPos(insn, mnode) + s" in method ${methodSignature(cnode, mnode)}"
+  }
+
+  final def insnPosInMethodSignature(insn: AbstractInsnNode, mnode: MethodNode, ownerIName: String): String = {
+    insnPos(insn, mnode) + s" in method ${methodSignature(ownerIName, mnode)}"
+  }
+
+} // end of class BCodeOptCommon
