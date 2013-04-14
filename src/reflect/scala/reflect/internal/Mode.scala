c039969e7a563b345fd054ffbb04742958697aa3
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-04-14 23:17:09 +0200

    a few (mostly internal) value-classes of the compiler become reallyStatic

--- src/reflect/scala/reflect/internal/Mode.scala
+++ src/reflect/scala/reflect/internal/Mode.scala
@@ -6,6 +6,7 @@
 package scala.reflect
 package internal
 
+@reallyStatic
 object Mode {
   private implicit def liftIntBitsToMode(bits: Int): Mode = apply(bits)
   def apply(bits: Int): Mode = new Mode(bits)
