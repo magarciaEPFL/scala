6f2c85ee02158fd10b9f340a6012df6b8b635f0f
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-04-14 18:07:24 +0200

    Late Anonymous-Closure Classes: GenBCode dehydrates them (+ TODO)
    Transform a Function node
      (x_1,...,x_n) => body
    of type
      FunctionN[T_1, .., T_N, R]
    into a Block expression of the form:
    
        {
          def hoisted(x_1: T_1, ..., x_N: T_n): R = body
    
          hoisted(zeroes-for-params-above).asInstanceOf[AbstractFunctionN[T_1, .., T_N, R]]
        }
    
    GenBCode will emit either:
    
      (a) an anonymous closure class and its instantiation ( under -closurify:delegating )
          or
      (b) a method handle given as constructor-argument to a closure instantiation ( under -closurify:MH )
    
    That way, the compiler has less work to do during specialize, explicitouter, erasure, and lambdalift, among others.
    
    The resulting anon-closure-classes have smaller constant pools, which significantly shrinks output size (good for mobile devices).
    Further classfile-size reductions can be obtained using -neo:o1 (ie intra-method optimizations, which lower the instruction count).
    
    TODO
      compiling test\files\continuations-run\t3223.scala
      with options -P:continuations:enable -Xpluginsdir build\quick\misc\scala-devel\plugins -Xplugin-require:continuations
      trips an assertion in GenBCode.

--- src/compiler/scala/tools/nsc/settings/ScalaSettings.scala
+++ src/compiler/scala/tools/nsc/settings/ScalaSettings.scala
@@ -205,6 +205,10 @@ trait ScalaSettings extends AbsScalaSettings
   val neo         = ChoiceSetting ("-neo", "choice of bytecode emitter", "Choice of bytecode emitter.",
                                    List("GenASM", "GenBCode", "o1"),
                                    "GenBCode") // TODO once merged into trunk "GenASM" should be the default
+  val closureConv = ChoiceSetting ("-closurify", "closure desugaring", "Bytecode-level representation of anonymous closures.",
+                                   List("traditional", "delegating"),
+                                   "delegating") // TODO once merged into trunk "traditional" should be the default
+
   // Feature extensions
   val XmacroSettings          = MultiStringSetting("-Xmacro-settings", "option", "Custom settings for macros.")
 
@@ -247,5 +251,25 @@ trait ScalaSettings extends AbsScalaSettings
    */
   def neoLevel: Int           = { if(neo.value.startsWith("o") && isBCodeActive) neo.value.substring(1).toInt else 0 }
   def isIntraMethodOptimizOn  = (neoLevel >= 1)
+
+  /*
+   *  Approaches to lower anonymous closures:
+   *
+   *    case "traditional"  => Good ol' dedicated inner class for each closure.
+   *                           Available under GenASM (the only option there), and also with GenBCode but only up to -neo:o1.
+   *
+   *    case "delegating"   => aka "Late-Closure-Classes" ie their creation is postponed (instead of UnCurry during GenBCode)
+   *                           thus lowering the working set during compilation.
+   *                           Allows closure-related optimizations (actually all optimization levels are supported).
+   *
+   *    case "MH" => A JSR 292 MethodHandle instance with bound arguments for captured environment (aka "partial application")
+   *                 is given as constructor-argument to a *standard* closure-class
+   *                 (thus doing away with the need for as many individual classes as closure definitions).
+   *                 Allows all optimization levels, -target must be jvm-1.7 or higher, and the backend must be GenBCode.
+   *
+   */
+  def isClosureConvTraditional = (closureConv.value == "traditional") || !isBCodeActive
+  def isClosureConvDelegating  = (closureConv.value == "delegating")  &&  isBCodeActive
+  def isClosureConvMH          = (closureConv.value == "MH")          &&  isBCodeActive
 
 }
