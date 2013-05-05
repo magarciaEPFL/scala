96f6538402220cfb41b519f692014c42d71f5d63
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-05-05 22:27:46 +0200

    Level 1 intra-method optimiz: compacting method-local vars
    "Compacting" of non-param local-vars involves:
       - detecting gaps in the index sequence, followed by
       - re-numbering that shifts indexes to close the gaps.
    
    As already hinted, "compacting" excludes parameters.
    Also, a local-var that at least gets initialized won't be removed.

--- src/compiler/scala/tools/nsc/backend/jvm/BCodeOptIntra.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/BCodeOptIntra.scala
@@ -438,6 +438,7 @@ abstract class BCodeOptIntra extends BCodeTypes {
   final class BCodeCleanser(cnode: asm.tree.ClassNode) extends QuickCleanser(cnode) {
 
     val unboxElider           = new asm.optimiz.UnBoxElider
+    val lvCompacter           = new asm.optimiz.LocalVarCompact
 
     /*
      *  The intra-method optimizations below are performed until a fixpoint is reached.
@@ -476,6 +477,7 @@ abstract class BCodeOptIntra extends BCodeTypes {
 
         if (full) {
           unboxElider.transform(cnode.name, mnode) // remove box/unbox pairs (this transformer is more expensive than most)
+          lvCompacter.transform(mnode)             // compact local vars, remove dangling LocalVariableNodes.
         }
 
         ifDebug { runTypeFlowAnalysis(mnode) }
