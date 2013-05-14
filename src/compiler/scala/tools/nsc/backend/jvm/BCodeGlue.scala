96bb44f005471b48befedb87dc488b3c6bf635ad
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-05-14 23:40:20 +0200

    uniquifying BType instances based on their (sort, offset, length)

--- src/compiler/scala/tools/nsc/backend/jvm/BCodeGlue.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/BCodeGlue.scala
@@ -69,13 +69,13 @@ abstract class BCodeGlue extends SubComponent {
               len += 1
             }
           }
-          new BType(asm.Type.ARRAY, off, len + 1)
+          getCanonical(asm.Type.ARRAY, off, len + 1)
         case 'L' =>
           len = 1
           while (chrs(off + len) != ';') {
             len += 1
           }
-          new BType(asm.Type.OBJECT, off + 1, len - 1)
+          getCanonical(asm.Type.OBJECT, off + 1, len - 1)
         // case '(':
         case _ =>
           assert(chrs(off) == '(')
@@ -83,7 +83,7 @@ abstract class BCodeGlue extends SubComponent {
           while (chrs(resPos) != ')') { resPos += 1 }
           val resType = getType(resPos + 1)
           val len = resPos - off + 1 + resType.len;
-          new BType(
+          getCanonical(
             asm.Type.METHOD,
             off,
             if (resType.hasObjectSort) {
@@ -93,6 +93,14 @@ abstract class BCodeGlue extends SubComponent {
             }
           )
       }
+    }
+
+    private def getCanonical(sort: Int, off: Int, len: Int): BType = {
+      val ca  = _root_.java.util.Arrays.copyOfRange(chrs, off, off + len)
+      assert(new String(chrs, off, len) == new String(ca))  // TODO debug
+      val n = global.newTypeName(ca)
+      // this time we've fielded on the canonical offset.
+      new BType(sort, n.start, n.length)
     }
 
     /* Params denote an internal name.
@@ -637,31 +645,7 @@ abstract class BCodeGlue extends SubComponent {
         return false
       }
       val t = o.asInstanceOf[BType]
-      if (this eq t) {
-        return true
-      }
-      if (sort != t.sort) {
-        return false
-      }
-      if (sort >= asm.Type.ARRAY) {
-        if (len != t.len) {
-          return false
-        }
-        // sort checked already
-        if (off == t.off) {
-          return true
-        }
-        var i = 0
-        while (i < len) {
-          if (chrs(off + i) != chrs(t.off + i)) {
-            return false
-          }
-          i += 1
-        }
-        // If we reach here, we could update the largest of (this.off, t.off) to match the other, so as to simplify future == comparisons.
-        // But that would require a var rather than val.
-      }
-      true
+      (hiPart == t.hiPart) && (off == t.off)
     }
 
     /*
@@ -670,16 +654,7 @@ abstract class BCodeGlue extends SubComponent {
      * can-multi-thread
      */
     override def hashCode(): Int = {
-      var hc = 13 * sort;
-      if (sort >= asm.Type.ARRAY) {
-        var i = off
-        val end = i + len
-        while (i < end) {
-          hc = 17 * (hc + chrs(i))
-          i += 1
-        }
-      }
-      hc
+      13 * hiPart + 17 * off
     }
 
     /*
