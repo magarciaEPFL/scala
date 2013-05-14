60b48d3539f3634ffd43b4b0f170c4f4023e74f2
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-05-14 23:30:48 +0200

    readability, elidedClasses map

--- src/compiler/scala/tools/nsc/backend/jvm/GenBCode.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/GenBCode.scala
@@ -198,10 +198,7 @@ abstract class GenBCode extends BCodeOptClosu {
        * the second condition holds when an intra-class optimization did the eliding
        * (during BCodeCleanser.cleanseClass(), running after whole-program).
        */
-      def wasElided = {
-        (plain == null) ||
-        elidedClasses.contains(lookupRefBType(plain.jclassName))
-      }
+      def forgetIt = { (plain == null) || wasElided(plain.jclassName) }
 
     }
     private val i3comparator = new _root_.java.util.Comparator[Item3] {
@@ -653,7 +650,7 @@ abstract class GenBCode extends BCodeOptClosu {
         }
         while (!followers.isEmpty && followers.peek.arrivalPos == expected) {
           val item = followers.poll
-          if (!item.wasElided) {
+          if (!item.forgetIt) {
             val outFolder = item.outFolder
             sendToDisk(item.mirror, outFolder)
             sendToDisk(item.plain,  outFolder)
