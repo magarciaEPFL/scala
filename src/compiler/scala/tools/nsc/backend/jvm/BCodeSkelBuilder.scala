74433f3253cd7695bcdac8e620af03ad30bf44a0
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-05-29 23:00:49 +0200

    less pressure on GenBCode's BType cache

--- src/compiler/scala/tools/nsc/backend/jvm/BCodeSkelBuilder.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/BCodeSkelBuilder.scala
@@ -555,7 +555,7 @@ abstract class BCodeSkelBuilder extends BCodeOptInter {
 
       methSymbol  = dd.symbol
       jMethodName = methSymbol.javaSimpleName.toString
-      returnType  = asmMethodType(dd.symbol).getReturnType
+      returnType  = asmMethodType(dd.symbol).returnType
       isMethSymStaticCtor = methSymbol.isStaticConstructor
       isMethSymBridge     = methSymbol.isBridge
 
