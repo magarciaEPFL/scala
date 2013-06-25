e438616a38afae5b0dd7e1f0952e6a46dea6b9de
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-06-26 00:05:28 +0200

    Level 1 intra-method optimiz: ConstantFolder
    Intra-method optimizations are performed until a fixpoint is reached.
    
    Constant folding comprises:
    
      - loads that are amenable to shorthand (e.g. ICONST_1) are rewritten
    
      - stores where the LHS already contains the value
        denoted by the RHS are replaced by DROP instruction
    
      - binary operations with constant result are left in place,
        but a DROP and a load of the constant-result are inserted
        rigth after them. Successive iterations of the fixpoint will
        reduce code to its final form.
    
      - conditional jumps, as well as lookup and table switches
        are simplified when their operand is constant.

--- src/compiler/scala/tools/nsc/backend/jvm/GenBCode.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/GenBCode.scala
@@ -111,6 +111,8 @@ abstract class GenBCode extends BCodeOptIntra {
     override def description = "Generate bytecode from ASTs using the ASM library"
     override def erasedTypes = true
 
+    val isOptimizRun  = settings.isIntraMethodOptimizOn
+
     // number of woker threads for pipeline-2 (the pipeline in charge of most optimizations except inlining).
     val MAX_THREADS = scala.math.min(
       4,
@@ -278,6 +280,8 @@ abstract class GenBCode extends BCodeOptIntra {
      */
     class Worker2 extends _root_.java.lang.Runnable {
 
+      val isIntraMethodOptimizOn = settings.isIntraMethodOptimizOn
+
       def run() {
 
         val id = java.lang.Thread.currentThread.getId
@@ -310,8 +314,15 @@ abstract class GenBCode extends BCodeOptIntra {
 
         val cnode   = item.plain
 
-        val essential = new EssentialCleanser(cnode)
-        essential.codeFixupDCE()    // the very least fixups that must be done, even for unoptimized runs.
+        val cleanser = new QuickCleanser(cnode)
+        cleanser.codeFixupDCE()       // the minimal fixups needed, even for unoptimized runs.
+        if (isOptimizRun) {
+          import asm.optimiz.Util
+          for(mnode <- cnode.toMethodList; if Util.hasBytecodeInstructions(mnode)) {
+            Util.computeMaxLocalsMaxStack(mnode)
+            cleanser.basicIntraMethodOpt(mnode)   // intra-method optimizations performed until a fixpoint is reached
+          }
+        }
 
         refreshInnerClasses(cnode)
 
