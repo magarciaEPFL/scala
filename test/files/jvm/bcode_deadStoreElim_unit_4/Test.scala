a853691ed225b11c345d4908610e38ca7f8aee21
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-08-28 18:46:12 +0200

    BCodeOpt building block: DeadStoreElimRef
    Perform dead-store elimination for local-vars
    holding references (dead-store elimination can be performed
    more aggresively for primitive values than for reference values).
    
    Details in the documentation for
    scala.tools.nsc.backend.bcode.DeadStoreElimRef

---
+++ test/files/jvm/bcode_deadStoreElim_unit_4/Test.scala
@@ -0,0 +1,63 @@
+import scala.tools.nsc.backend.bcode.DeadStoreElimRef
+import scala.tools.nsc.backend.bcode.Util
+import scala.tools.partest.BytecodeTest
+import scala.tools.asm
+import scala.collection.JavaConverters._
+
+import scala.tools.asm.Opcodes
+
+/*
+ *   (a) reassignment from same ===> drop instead of store
+ */
+object Test extends BytecodeTest {
+
+  def show: Unit = {
+    val t   = transformed(before())
+    val isa = wrapped(t)
+    val isb = wrapped(after())
+    assert(isa == isb)
+  }
+
+  def wrapped(m: asm.tree.MethodNode) = instructions.fromMethod(m)
+
+  def mkMethodNode = {
+    new asm.tree.MethodNode(
+      Opcodes.ACC_PUBLIC,
+      "m",
+      "()V",
+      null, null
+    )
+  }
+
+  def before(): asm.tree.MethodNode = {
+    val m  = mkMethodNode
+    m.visitVarInsn(Opcodes.ALOAD, 0)
+    m.visitVarInsn(Opcodes.ASTORE, 1)
+    m.visitVarInsn(Opcodes.ALOAD, 0)
+    m.visitVarInsn(Opcodes.ASTORE, 1)
+
+    m.visitInsn(Opcodes.RETURN)
+
+    m
+  }
+
+  def after(): asm.tree.MethodNode = {
+    val m  = mkMethodNode
+    m.visitVarInsn(Opcodes.ALOAD, 0)
+    m.visitInsn(Opcodes.POP)
+    m.visitVarInsn(Opcodes.ALOAD, 0)
+    m.visitInsn(Opcodes.POP)
+    m.visitInsn(Opcodes.RETURN)
+
+    m
+  }
+
+  def transformed(input: asm.tree.MethodNode): asm.tree.MethodNode = {
+    val tr = new DeadStoreElimRef
+    Util.computeMaxLocalsMaxStack(input)
+    do { tr.transform("C", input) } while (tr.changed)
+
+    input
+  }
+
+}
