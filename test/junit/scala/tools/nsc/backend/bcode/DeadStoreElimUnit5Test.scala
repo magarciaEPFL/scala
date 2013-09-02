5ac6e22b7f3ab853bc5e587272881d1d023241d8
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-09-02 23:03:13 +0200

    BCodeOpt building block: DeadStoreElimRef
    Perform dead-store elimination for local-vars
    holding references (dead-store elimination can be performed
    more aggresively for primitive values than for reference values).
    
    Details in the documentation for
    scala.tools.nsc.backend.bcode.DeadStoreElimRef

---
+++ test/junit/scala/tools/nsc/backend/bcode/DeadStoreElimUnit5Test.scala
@@ -0,0 +1,69 @@
+package scala.tools.nsc.backend.bcode
+
+import org.junit.Assert._
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+import scala.tools.asm
+import scala.collection.JavaConverters._
+
+import scala.tools.asm.Opcodes
+
+/*
+ *   (b) assign RHS produced-by-ACONST_NULL to uninitialized LHS that goes unread ===> drop instead of store
+ *       (assign null to null already simplified by NullnessPropagator)
+ */
+@RunWith(classOf[JUnit4])
+class DeadStoreElimUnit5Test {
+
+  @Test
+  def show: Unit = {
+    val t   = transformed(before())
+    val isa = wrapped(t)
+    val isb = wrapped(after())
+    assert(isa == isb)
+  }
+
+  def wrapped(m: asm.tree.MethodNode) = {
+    Util.computeMaxLocalsMaxStack(m)
+    Util.textify(m)
+  }
+
+  def mkMethodNode = {
+    new asm.tree.MethodNode(
+      Opcodes.ACC_PUBLIC,
+      "m",
+      "()V",
+      null, null
+    )
+  }
+
+  def before(): asm.tree.MethodNode = {
+    val m  = mkMethodNode
+    m.visitInsn(Opcodes.ACONST_NULL)
+    m.visitVarInsn(Opcodes.ASTORE, 1)
+
+    m.visitInsn(Opcodes.RETURN)
+
+    m
+  }
+
+  def after(): asm.tree.MethodNode = {
+    val m  = mkMethodNode
+    m.visitInsn(Opcodes.ACONST_NULL)
+    m.visitInsn(Opcodes.POP)
+    m.visitInsn(Opcodes.RETURN)
+
+    m
+  }
+
+  def transformed(input: asm.tree.MethodNode): asm.tree.MethodNode = {
+    val tr = new DeadStoreElimRef
+    Util.computeMaxLocalsMaxStack(input)
+    do { tr.transform("C", input) } while (tr.changed)
+
+    input
+  }
+
+}
