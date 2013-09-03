492202d09ae46b163fc9c9da21af604c789a8708
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-09-03 20:47:15 +0200

    BCodeOpt building block: CopyPropagator
    Replaces the last link in a chain of data accesses
    (involving local-variables only)
    by a direct access to the chain-start.
    
    Details in the documentation for
    scala.tools.nsc.backend.bcode.CopyPropagator

---
+++ test/files/jvm/bcode_copyPropag_unit_1/Test.scala
@@ -0,0 +1,38 @@
+import scala.tools.nsc.backend.bcode.CopyPropagator
+import scala.tools.partest.BytecodeTest
+import scala.tools.asm
+import scala.collection.JavaConverters._
+
+import scala.tools.asm.Opcodes
+
+object Test extends BytecodeTest {
+
+  val inputClass = "Foo_1"
+
+  def show: Unit = {
+    val classNode  = loadClassNode(inputClass)
+    compare(classNode, "pre1", "post1")
+    compare(classNode, "pre2", "post2")
+  }
+
+  def compare(classNode: asm.tree.ClassNode, strA: String, strB: String) {
+    val pre  = getMethod(classNode, "pre1")
+    val post = getMethod(classNode, "post1")
+
+    val t   = transformed(pre)
+    val isa = wrapped(t)
+    val isb = wrapped(post)
+
+    assert(isa == isb)
+  }
+
+  def wrapped(m: asm.tree.MethodNode) = instructions.fromMethod(m)
+
+  def transformed(input: asm.tree.MethodNode): asm.tree.MethodNode = {
+    val tr = new CopyPropagator
+    do { tr.transform(inputClass, input) } while (tr.changed)
+
+    input
+  }
+
+}
