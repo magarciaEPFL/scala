46af79c5fb99f87395d735278821211fb4f179d8
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-09-05 21:07:09 +0200

    BCodeOpt: NullnessPropagator takes module load for non-null
    The load of a MODULE$ field won't be elided.
    However, the non-nullness tracked for that value might lead later on
    to control-flow simplification.

--- test/junit/scala/tools/nsc/backend/bcode/NullnessPropagatorTest.scala
+++ test/junit/scala/tools/nsc/backend/bcode/NullnessPropagatorTest.scala
@@ -165,6 +165,37 @@ class NullnessPropagatorTest {
     compareMethods(before, after)
   }
 
+  /*
+   *    (f) module load is taken for non-null. In detail, the field-load won't be elided.
+   *        Instead, control-flow can be simplified based on the non-nullness information.
+   */
+  @Test
+  def nonNullModuleLoadTest: Unit = {
+    val before: asm.tree.MethodNode = {
+      val m  = mkMethodNode
+      m.visitFieldInsn(Opcodes.GETSTATIC, "scala/Predef$", "MODULE$", "Lscala/Predef$;")
+      val L = new asm.Label
+      m.visitJumpInsn(Opcodes.IFNONNULL, L)
+      m.visitLabel(L)
+      m.visitInsn(Opcodes.RETURN)
+
+      m
+    }
+    val after: asm.tree.MethodNode = {
+      val m  = mkMethodNode
+      m.visitFieldInsn(Opcodes.GETSTATIC, "scala/Predef$", "MODULE$", "Lscala/Predef$;")
+      val L = new asm.Label
+      m.visitInsn(Opcodes.POP)
+      m.visitJumpInsn(Opcodes.GOTO, L)
+      m.visitLabel(L)
+      m.visitInsn(Opcodes.RETURN)
+
+      m
+    }
+
+    compareMethods(before, after)
+  }
+
   private def mkMethodNode = {
     new asm.tree.MethodNode(Opcodes.ACC_PUBLIC, "m", "()V", null, null)
   }
