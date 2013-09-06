7708c098b82d5e51ef47e86de21cda5ccd493ec1
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-09-06 23:15:55 +0200

    Late Anonymous-Closure Classes: GenBCode dehydrates them (+ TODO)
    Transform a Function node
      (x_1,...,x_n) => body
    of type
      FunctionN[T_1, .., T_N, R]
    into a Block expression of the form:
    
        {
          def hoisted(x_1: T_1, ..., x_N: T_n): R = body
    
           ScalaRunTime.lccMethodDisguiser(
             hoisted(zeroes-for-params-above)
           ).asInstanceOf[AbstractFunctionN[T_1, .., T_N, R]]
        }
    
    where `ScalaRunTime.lccMethodDisguiser()` denotes a synthetic, artifact
    method symbol created in Definitions, that thus doesn't occur
    in user programs.
    
    In place of the above, GenBCode will emit
    under `-Yclosurify:delegating` the following:
      - an anonymous closure class, and
      - its instantiation.
    
    That way, the compiler has less work to do during specialize,
    explicitouter, erasure, and lambdalift, among others.
    
    The resulting anon-closure-classes have smaller constant pools,
    which significantly shrinks output size (good for mobile devices).
    Further classfile-size reductions can be obtained using -neo:o1
    (ie intra-method optimizations, which lower the instruction count).
    
    TODO an assertion is tripped in GenBCode upon compiling
        test/files/continuations-run/t3223.scala
      with options
        -P:continuations:enable -Xpluginsdir build/quick/misc/scala-devel/plugins -Xplugin-require:continuations

--- src/compiler/scala/tools/nsc/backend/jvm/BCodeTFA.scala
+++ src/compiler/scala/tools/nsc/backend/jvm/BCodeTFA.scala
@@ -18,7 +18,7 @@ import scala.collection.{ immutable, mutable }
  *  @version 1.0
  *
  */
-abstract class BCodeTFA extends BCodeHelpers with BytecodeWriters {
+abstract class BCodeTFA extends BCodeOptCommon with BytecodeWriters {
 
   import global._
 
