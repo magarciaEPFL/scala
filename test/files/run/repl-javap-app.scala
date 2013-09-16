580fddd57a35a6294b420ce2b6f1668149bda950
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-09-16 13:01:39 +0200

    check file hardcodes too many details about constant pool layout
    The check file contains output obtained via `:javap -app MyApp$`
    which includes constant pool indices. Compiling the test
    under -Ybackend:GenBCode removes redundant code,
    with the consequence that the constant pool isn't laid out
    anymore as for classfiles emitted by GenASM (in general
    and in the case at hand).

--- test/files/run/repl-javap-app.scala
+++ test/files/run/repl-javap-app.scala
@@ -5,6 +5,13 @@ object MyApp extends App {
   Console println "Hello, delayed world."
 }
 
+// The check file contains output obtained via `:javap -app MyApp$`
+// which includes constant pool indices. Compiling the test
+// under -Ybackend:GenBCode removes redundant code,
+// with the consequence that the constant pool isn't laid out
+// anymore as for classfiles emitted by GenASM (in general
+// and in the case at hand).
+
 object Test extends ReplTest {
   def code = ":javap -app MyApp$"
 }
