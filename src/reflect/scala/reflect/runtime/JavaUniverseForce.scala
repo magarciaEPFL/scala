cfe61f6bdb3bbf082f38288423dc11429b51b7ad
Miguel Garcia <miguelalfredo.garcia@epfl.ch>
2013-12-09 21:58:42 +0100

    Late Anonymous-Closure Classes: GenBCode dehydrates them (+ TODO)
    Transform a Function node
      (x_1,...,x_n) => body
    of type
      FunctionN[T_1, .., T_N, R]
    into a Block expression of the form:
    
        {
          def hoisted(x_1: T_1, ..., x_N: T_n): R = body
    
           ScalaRunTime.lccMethodDisguiser(
             hoisted(zeroes-for-params-above)
           ).asInstanceOf[AbstractFunctionN[T_1, .., T_N, R]]
        }
    
    where `ScalaRunTime.lccMethodDisguiser()` denotes a synthetic, artifact
    method symbol created in Definitions, that thus doesn't occur
    in user programs.
    
    In place of the above, GenBCode will emit
    under `-Yclosurify:delegating` the following:
      - an anonymous closure class, and
      - its instantiation.
    
    That way, the compiler has less work to do during specialize,
    explicitouter, erasure, and lambdalift, among others.
    
    The resulting anon-closure-classes have smaller constant pools,
    which significantly shrinks output size (good for mobile devices).
    Further classfile-size reductions can be obtained using -Ybackend:o1
    (ie intra-method optimizations, which lower the instruction count).
    
    TODO an assertion is tripped in GenBCode upon compiling
        test/files/continuations-run/t3223.scala
      with options
        -P:continuations:enable -Xpluginsdir build/quick/misc/scala-devel/plugins -Xplugin-require:continuations

--- src/reflect/scala/reflect/runtime/JavaUniverseForce.scala
+++ src/reflect/scala/reflect/runtime/JavaUniverseForce.scala
@@ -263,6 +263,7 @@ trait JavaUniverseForce { self: runtime.JavaUniverse  =>
     definitions.UnqualifiedOwners
     definitions.PredefModule
     definitions.SpecializableModule
+    definitions.lccDisguiserMethod
     definitions.ScalaRunTimeModule
     definitions.SymbolModule
     definitions.StringAddClass
